d58d260246479b736fda8db35c4cfdd42d077235
Furqon Aji <furqonajiy@gmail.com>
2023-12-05 21:39:25 +0700

    Add bisainvoice v1

--- generator/bisainvoice/tokopedia/v1.py
+++ generator/bisainvoice/tokopedia/v1.py
@@ -9,19 +9,19 @@ def generate_bisainvoice(tkp_file, df):
     logging.info("Generate BisaInvoice from {0} ({1} rows)".format(tkp_file, len(df)))
 
     # Remove Duplicate and Null Nomor Invoice
-    df = df.dropna(axis=0, subset=['Nomor Invoice'])
-    df = df.drop_duplicates(subset=['Nomor Invoice'])
+    df = df.dropna(axis=0, subset=['Invoice'])
+    df = df.drop_duplicates(subset=['Invoice'])
 
     # Add New Column contain 'Tokopedia'
     df['Marketplace'] = 'Tokopedia'
 
     # Set Biaya Non Tunai to 0
-    df.loc[(df['Biaya Pengiriman Tunai (IDR)'] == 'Non Tunai'), 'Biaya Pengiriman Tunai (IDR)'] = 0
-    df.loc[(df['Biaya Asuransi Pengiriman (IDR)'] == 'Non Tunai'), 'Biaya Asuransi Pengiriman (IDR)'] = 0
+    # df.loc[(df['Biaya Pengiriman Tunai (IDR)'] == 'Non Tunai'), 'Biaya Pengiriman Tunai (IDR)'] = 0
+    # df.loc[(df['Biaya Asuransi Pengiriman (IDR)'] == 'Non Tunai'), 'Biaya Asuransi Pengiriman (IDR)'] = 0
 
     # Select Needed Column
-    df = df[['Tanggal Pembayaran', 'Marketplace', 'Nomor Invoice', 'Biaya Pengiriman Tunai (IDR)',
-             'Biaya Asuransi Pengiriman (IDR)']]
+    df = df[['Payment Date', 'Marketplace', 'Invoice', 'Shipping Price + fee (Rp.)',
+             'Insurance (Rp.)']]
 
     # Change Column Name
     df.columns = ['Tanggal', 'Marketplace', 'Invoice', 'Ongkir', 'Asuransi']
